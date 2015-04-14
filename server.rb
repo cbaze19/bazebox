617a31c20c5afc04a34306ae82ec958f9f2b806d
Caleb Baze <cbaze19@gmail.com>
2015-04-14 16:00:01 -0500

    a few edits

--- server.rb
+++ server.rb
@@ -1,4 +1,11 @@
+# Main Server File
+# Responsible for main routing of app
+
 require 'sinatra'
+
+# Sets server to run in production mode
+# Do this to be able to access app via external IP
+# set :environment, :production
 
 use Rack::Auth::Basic, "Restricted Area" do |username, password|
   username == '' and password == ''
